2ebc4774fe3a0383fece01f8fb03f24d4974562e
Chris Palka <cpalka87@gmail.com>
2019-02-04 17:43:13 -0800

    Install devise gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,7 +53,7 @@ end
 puts "9 portfolio items created"
 
 3.times do |technology|
-	Portfolio.last.technologies.create!(
+	Portfolio.last.technologies.create!(		
 			name: "Technology #{technology}"
 		)
 end
