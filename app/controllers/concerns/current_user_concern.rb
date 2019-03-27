8fec2435e93795e7f766900ed9173b5bf6ea8971
Chris Palka <cpalka87@gmail.com>
2019-03-26 19:51:52 -0700

    Add to current_user_concern to facilitate successful redirect to original location after login, logout, and sign-up

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -1,6 +1,10 @@
 module CurrentUserConcern
 	extend ActiveSupport::Concern
 
+	included do
+		before_action :store_current_location, :unless => :devise_controller?
+	end
+
  	def current_user
  		super || guest_user
  	end
@@ -13,5 +17,15 @@ module CurrentUserConcern
  		guest.email = "guest@example.com"
  		guest
  	end
+
+ 	private
+  
+ 	def store_current_location
+ 		store_location_for(:user, request.url)
+	end
+
+	def after_sign_out_path_for(resource)
+		request.referrer || root_path
+	end
 
 end
