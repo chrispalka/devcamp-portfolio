d49bb8a7fc41b7c618b0bf432e3c7a58b24c55d9
Chris Palka <cpalka87@gmail.com>
2019-03-27 21:48:47 -0700

    Build out topic views

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,17 @@
+class TopicsController < ApplicationController
+	layout 'blog'
+
+  def index
+  	@topics = Topic.all
+  end
+
+  def show
+  	@topic = Topic.find(params[:id])
+
+  	if logged_in?(:site_admin)
+      @blogs = @topic.blogs.page(params[:page]).per(5).recent_blogs
+    else
+      @blogs = @topic.blogs.Published.page(params[:page]).per(5).recent_blogs
+    end
+  end
+end
